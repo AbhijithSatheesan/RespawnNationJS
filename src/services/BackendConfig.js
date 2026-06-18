aad970be0ed7ba90b2c83855e7e0c83f7a79e60c
ms_abhijith <abhijithms9207@gmail.com>
2026-06-18 18:45:03 +0530

    api changed

--- src/services/BackendConfig.js
+++ src/services/BackendConfig.js
@@ -1,13 +1,13 @@
-const django_url = 'http://localhost:8000/api/';
+// const django_url = 'http://localhost:8000/api/';
 
-export const django_api_url = django_url
+// export const django_api_url = django_url
 
-export const django_media_url = 'http://localhost:8000'
+// export const django_media_url = 'http://localhost:8000'
 
 
 
-// export const django_url = import.meta.env.VITE_DJANGO_API_URL
-// export const django_api_url = import.meta.env.VITE_DJANGO_API_URL
-// export const django_media_url = import.meta.env.VITE_MEDIA_URL
+export const django_url = import.meta.env.VITE_DJANGO_API_URL
+export const django_api_url = import.meta.env.VITE_DJANGO_API_URL
+export const django_media_url = import.meta.env.VITE_MEDIA_URL
 
 
